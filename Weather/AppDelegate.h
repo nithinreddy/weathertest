83c24a28b73425046eff1c0881fa70bbe19368f4
Nithin Reddy <nithinredd@gmail.com>
2016-03-14 07:36:14 +0530

    Initial Commit

---
+++ Weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weather
+//
+//  Created by Nithin Reddy on 14/03/16.
+//  Copyright © 2016 Povio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weather
+//
+//  Created by Nithin Reddy on 14/03/16.
+//  Copyright © 2016 Povio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
